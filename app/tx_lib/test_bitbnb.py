f6eb59141ab3937986eea89d8826e9ca3e3e9c8e
Jeff Tipps <jeff.tipps@ni.com>
2022-11-23 11:51:51 -0600

    changed ordering

--- app/tx_lib/test_bitbnb.py
+++ app/tx_lib/test_bitbnb.py
@@ -3,8 +3,8 @@ from Bitbnb.RedeemScript import RedeemScript
   
 refund_addr = 'moZvNSDyo9HuLemFHdKvW9d5rTRt2QXMUy'
 owner_addr = 'mvNR1qGaMR7jdmSsvFQf6nsXo7bb1ofn2M'
-serial_script = '76a9145850a5cfd6254cd20b00493a1fd063c166043adc8763ac67034abd24b17576a914a2ec72b722b3840c7f74a38a1a7482f91e5852fa88ac68'
 valid_height = 2407754
+serial_script = '76a9145850a5cfd6254cd20b00493a1fd063c166043adc8763ac67034abd24b17576a914a2ec72b722b3840c7f74a38a1a7482f91e5852fa88ac68'
 
 class TestRedeemScript:   
     def test_can_serialize(self):
